5664b0df8afa7c5a273775a49ccd50d3c7b26b98
kwkim0208 <ruddns0208@gmail.com>
2023-07-23 09:33:47 +0900

    빌더패턴을 이용한 객체 생성 추가

--- src/main/java/kr/com/devlog/common/ExceptionController.java
+++ src/main/java/kr/com/devlog/common/ExceptionController.java
@@ -18,7 +18,7 @@ public class ExceptionController {
 
     public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
         //Me
-        ErrorResponse errorResponse = new ErrorResponse("400", "잘못된 요청입니다.");
+        ErrorResponse errorResponse = ErrorResponse.builder().code("400").message("잘못된 요청입니다.").build();
         for (FieldError fieldError : e.getFieldErrors()) {
             errorResponse.addValidation(fieldError.getField(),fieldError.getDefaultMessage());
         }
--- src/main/java/kr/com/devlog/response/ErrorResponse.java
+++ src/main/java/kr/com/devlog/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package kr.com.devlog.response;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -7,14 +8,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Data
-@RequiredArgsConstructor
 public class ErrorResponse {
     private final String code;
-    private final  String message;
-    private Map<String,String> validation=new HashMap<>();
+    private final String message;
+    private Map<String, String> validation = new HashMap<>();
 
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
     public void addValidation(String fieldName, String errorMessage) {
-        this.validation.put(fieldName,errorMessage);
+        this.validation.put(fieldName, errorMessage);
 
     }
 }
